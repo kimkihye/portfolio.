8f8e8b2ce1318777af783385b6ddc283217fb03a
kimkihye <yunej170@gmail.xom>
2023-09-15 23:43:35 +0900

    portfolio.

--- script/location.js
+++ script/location.js
@@ -1,3 +1,22 @@
+//헤더 메뉴
+//변수설정
+const btnCall = document.querySelector(".btnCall");
+const menuMo = document.querySelector(".menuMo");
+
+//이벤트 바인딩
+
+//btnCall을 클릭할때
+
+btnCall.onclick = function(e){
+    //링크이동금지
+    e.preventDefault();
+
+    //btnCall에 on이 있으면 제거하고, 없으면 추가
+    btnCall.classList.toggle("on");
+    //menuMo에 on이 있으면 제거하고, 없으면 추가
+    menuMo.classList.toggle('on');
+}
+
 //992c3fa35a12c362af0414f562a235f7
 var container = document.getElementById('#map'); //지도를 담을 영역의 DOM 레퍼런스
 //교통정보를 보게하는 버튼
